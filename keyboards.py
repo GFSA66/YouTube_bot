908ea7c4d2411bb7253771921879f43d37bcee7e
Сорокин Артем <145757128+GFSA66@users.noreply.github.com>
2024-04-10 22:27:00 +0300

    1.5.0

--- keyboards.py
+++ keyboards.py
@@ -7,9 +7,11 @@ menu = types.ReplyKeyboardMarkup()
 menu.add(btn1,btn2)
 
 resolutions = ['144', '240', '360', '480', '720', '1080']
-download_choose = ['video','audio']
+download_choose = ['Video','Audio']
+emojies = ["🎦",'🎧']
 
 res = types.InlineKeyboardMarkup(row_width=2)
-
+i=0
 for resolution in download_choose:
-    res.add(types.InlineKeyboardButton(resolution, callback_data=resolution))
+    res.add(types.InlineKeyboardButton(emojies[i]+resolution, callback_data=resolution))
+    i+=1
--- main.py
+++ main.py
@@ -5,11 +5,24 @@ from telebot.async_telebot import AsyncTeleBot
 from time import sleep 
 from datetime import datetime
 import asyncio
-from pytube import YouTube, Stream
-from youtubesearchpython import VideosSearch
+from pytube import *
+from youtubesearchpython import *
 import os
 import requests
 from io import BytesIO
+from innertube import *
+import logging
+from typing import Any, Callable, Dict, List, Optional
+
+import pytube
+import pytube.exceptions as exceptions
+from pytube import extract, request
+from pytube import Stream, StreamQuery
+from pytube.helpers import install_proxy
+from pytube.innertube import InnerTube
+from pytube.metadata import YouTubeMetadata
+from pytube.monostate import Monostate
+
 
 bot = AsyncTeleBot(TOKEN)
 # updater = Updater(token='TOKEN', request_kwargs={'read_timeout': 1000, 'connect_timeout': 1000})
@@ -38,10 +51,18 @@ async def start(message: types.Message):
 	print(ids,names)
 	await bot.send_message(message.chat.id, "Привет! Здесь ты сможешь скачать видео с YouTube или анимешку, а если что-то не понятно то просто напиши'/help'",reply_markup=keyboards.menu)
 	await bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAIrIGYTCo3qRYeGuVFXVYABqO_m3sNBAAIFAAPANk8T-WpfmoJrTXU0BA")
+@bot.message_handler(commands = ["help"])
+async def help(message: types.Message):
+	await bot.send_message(message.chat.id, '''Привет! Здесь ты сможешь скачать видео с YouTube или анимешку, \n\nВидео не должно быть: 
+						Слишком длинным
+						Высокого качества(2к или 4к)
+						Просто жди пока оно загрузиться
+						Если что то пойдёт не так, то тебе об этом напишет''',reply_markup=keyboards.menu)
+
 
 @bot.message_handler()
 async def handle_youtube_link(message: types.Message):
-	searchList = VideosSearch(message.text, limit = 5).result()['result']
+	searchList = VideosSearch(message.text, limit = 10).result()['result']
 	keyboard = list()
 	for element in searchList:
 		a = [types.InlineKeyboardButton(text=element['title'], callback_data=element['link'])]
@@ -49,21 +70,26 @@ async def handle_youtube_link(message: types.Message):
 	choose_markup =  types.InlineKeyboardMarkup(keyboard,1)
 	await bot.send_message(message.chat.id, 'Найденные варианты:', reply_markup=choose_markup)
 
+
 @bot.callback_query_handler(func = lambda call:True)
 async def call_streams(call: types.CallbackQuery):
-	if "youtube.com" in call.data or "youtu.be" in call.data:
-		global data1
-		print(call.data)
-		data1 = call.data
-		await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id, text=call.data) #добавить установку аудио или видео
-		await bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.id,reply_markup=keyboards.res)
-	if call.data == 'video':
-		call.data = data1
-		await download_youtube_video(call.data, call.message.chat.id)
-	if call.data == 'audio':
-		call.data = data1
-		await download_youtube_audio_only(call.data, call.message.chat.id)
-	
+	global data1
+	try:
+		if "youtube.com" in call.data or "youtu.be" in call.data:
+			print(call.data)
+			data1 = call.data
+			await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id, text=call.data) #добавить установку аудио или видео
+			await bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.id,reply_markup=keyboards.res)
+		if call.data == 'Video':
+			call.data = data1
+			await download_youtube_video(call.data, call.message.chat.id)
+		if call.data == 'Audio':
+			call.data = data1
+			await download_youtube_audio_only(call.data, call.message.chat.id)
+	except Exception as e:
+		print(e)
+		await bot.send_message(chat_id=call.message.chat.id, message_id=call.message.id, text=f"Ошибка при скачивании или отправке видео: \n{str(e)}")
+
 async def download_youtube_video(url, chat_id):
 	try:
 		yt = YouTube(url)
