d32511618c6239ddf68c0be22c229d8b50077dba
Сорокин Артем <145757128+GFSA66@users.noreply.github.com>
2024-04-11 13:56:55 +0300

    1.5.2

--- main.py
+++ main.py
@@ -10,18 +10,7 @@ from youtubesearchpython import *
 import os
 import requests
 from io import BytesIO
-from innertube import *
-import logging
-from typing import Any, Callable, Dict, List, Optional
-
-import pytube
-import pytube.exceptions as exceptions
-from pytube import extract, request
-from pytube import Stream, StreamQuery
-from pytube.helpers import install_proxy
-from pytube.innertube import InnerTube
-from pytube.metadata import YouTubeMetadata
-from pytube.monostate import Monostate
+from pytube.cli import on_progress
 
 
 #  https://t.me/Youtubescr_bot
