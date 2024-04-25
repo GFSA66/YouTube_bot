1eeaea94824156381f32e4fba39beb5d9f8a7baf
GFSA66 <gf66sorokina@gmail.com>
2024-04-25 17:43:58 +0300

    1.2.0

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 TOKEN = '6953575470:AAGM5ZNM7aB72PsOkoHpOmGjgmTRbKpEJI8'
 
 # https://t.me/Youtubescr_bot 6953575470:AAGM5ZNM7aB72PsOkoHpOmGjgmTRbKpEJI8
+
+# https://t.me/SearchingInformationPerfectBot 7012163402:AAGLoC51q0QdJd9qoShA-w_bz3XiloToBSk
